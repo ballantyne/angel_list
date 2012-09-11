e9bf00cb6759f127c5f0a52bd48714141205ba67
Scott Ballantyne <ussballantyne@gmail.com>
2012-09-11 16:02:28 +0800

    i think that this is finished

--- lib/angel_list/feed.rb
+++ lib/angel_list/feed.rb
@@ -1,4 +1,11 @@
 module AngelList
   class Feed < Base
+    def all(options={})
+      get('https://api.angel.co/1/feed', options)
+    end
+    
+    def find(id)
+      get('https://api.angel.co/1/feed/'+id.to_s)
+    end
   end
 end
